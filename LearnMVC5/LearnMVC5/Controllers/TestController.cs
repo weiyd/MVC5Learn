97c69bade31571f56969a5b9e74f9195f9fabfc0
weiyingda <727101923@qq.com>
2018-03-03 09:37:23 +0800

    Lab1 Controller直接返回字符串在View显示

---
+++ LearnMVC5/LearnMVC5/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearnMVC5.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public string GetString()
+        {
+            return "控制器直接返回的字符串";
+        }
+    }
+}
